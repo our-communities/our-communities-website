e5c2507ce5950fb523816f33d1d02ee5ff67456d
Tony Edwards <tony.edwards@gmail.com>
2020-05-12 18:40:43 +0100

    Moves jquery into the project

--- gulp-tasks/javascript.js
+++ gulp-tasks/javascript.js
@@ -45,17 +45,18 @@ gulp.task('mc-js', function() {
   let outDirPath = isProd ? '/opt/build/repo/_site/assets/js' : '_site/assets/js';
   let dirPath = isProd ? '/opt/build/repo/assets/js' : 'assets/js';
 
-  // return browserify(`${inDirPath}`, {debug: true, extensions: ['es6']})
-  //   .transform('babelify', {presets: ['es2015']})
-  //   .bundle()
-  //   .on('error', handleErrors)
-  //   .pipe(source('mc-bundle.js'))
-  //   .pipe(gulpif(isProd, remove_logging()))
-  //   .pipe(buffer())
-  //   .pipe(uglify())
-  //   .pipe(gulp.dest(outDirPath))
-  //   .pipe(browserSync.reload({stream: true}))
-  //   .pipe(gulp.dest(dirPath));
+  return gulp.src(inDirPath)
+      .pipe(gulp.dest(outDirPath))
+      .pipe(gulp.dest(dirPath));
+});
+
+gulp.task('jquery', function() {
+  let isProd = process.env.CONTEXT ? true : false;
+
+  let inDirPath = isProd ? '/opt/build/repo/_js/jquery.js' : '_js/jquery.js';
+  let outDirPath = isProd ? '/opt/build/repo/_site/assets/js' : '_site/assets/js';
+  let dirPath = isProd ? '/opt/build/repo/assets/js' : 'assets/js';
+
   return gulp.src(inDirPath)
       .pipe(gulp.dest(outDirPath))
       .pipe(gulp.dest(dirPath));
